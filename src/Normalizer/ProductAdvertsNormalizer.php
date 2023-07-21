26cf942f5d868d47c6737ea66b814959f36f24a5
kent@legion <164713332@qq.com>
2023-07-21 11:02:40 +0000

    Drupal10 installed successfully.

--- src/Normalizer/ProductAdvertsNormalizer.php
+++ src/Normalizer/ProductAdvertsNormalizer.php
@@ -2,29 +2,37 @@
 
 namespace Drupal\product_adverts\Normalizer;
 
-use Drupal\commerce_product\Entity\ProductInterface;
 use Drupal\product_adverts\Entity\ProductAdverts;
 use Drupal\serialization\Normalizer\ContentEntityNormalizer;
 
+/**
+ * {@inheritdoc}
+ */
 class ProductAdvertsNormalizer extends ContentEntityNormalizer {
 
-  public function supportsNormalization($data, $format = NULL) {
+  /**
+   * {@inheritdoc}
+   */
+  public function supportsNormalization($data, $format = NULL, array $context = []): bool {
     if ($data instanceof ProductAdverts) {
       $route_name = \Drupal::routeMatch()->getRouteName();
       return $route_name === 'view.api_product_adverts.rest_export_1';
+    }
+    else {
+      return FALSE;
     }
   }
 
   /**
    * {@inheritdoc}
    */
-  public function normalize($entity, $format = NULL, array $context = []) {
+  public function normalize($entity, $format = NULL, array $context = []): float|array|\ArrayObject|bool|int|string|null {
 
     $data = parent::normalize($entity, $format, $context);
-    /** @var ProductInterface $product */
+    /** @var \Drupal\commerce_product\Entity\ProductInterface $product */
     $product = $entity->get('product_id')->entity;
 
-    $image = null;
+    $image = NULL;
     if ($product->hasField('image')) {
       $image = file_create_url($product->get('image')->entity->getFileUri());
     }
@@ -33,7 +41,7 @@ class ProductAdvertsNormalizer extends ContentEntityNormalizer {
       'id' => $product->id(),
       'title' => $product->getTitle(),
       'image' => $image,
-      'price' => $product->getDefaultVariation()->getPrice()->toArray()
+      'price' => $product->getDefaultVariation()->getPrice()->toArray(),
     ];
 
     $this->addCacheableDependency($context, $product);
